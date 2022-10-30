3528ba2e828b83b68d3e8a777b877fe0b6b2c52f
Svetlanahrhr <maadlana@mail.ru>
2022-10-30 13:55:28 +0400

    refactor: change tag selectors according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    listItem.classList.add("list-item");
+
     label.innerText=taskString;
     label.className='task';
 
